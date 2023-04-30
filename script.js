9b6c444d5e895a79e77287e95fb7480df30758f7
vxnuaj <jv.100420@gmail.com>
2023-04-30 03:00:29 -0400

    dark mode?

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementsById('toggledark');
+const body = document.querySelector('body');
+
+toolbar.addEventListener('click', function(){
+    this.classList.toggle('bi-sun');
+})
