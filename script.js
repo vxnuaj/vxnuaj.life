6c38fffbb1104a9b0997f4408ee9cad752f492ef
vxnuaj <jv.100420@gmail.com>
2023-05-26 16:24:47 -0400

    a whole shit ton

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ toggle.addEventListener('click', function() {
       ppcrd.style.background = 'black'
       pwcrd.style.color = 'white'
       pwcrd.style.background = 'black'
+      
     } 
     else {
       body.style.background = 'white';
@@ -101,3 +102,81 @@ lifetxt.addEventListener('animationend', function() {
     runtxt.style.animation = 'typing 1.5s steps(16), cursor .4s step-end infinite alternate';
     runtxt.style.color = 'inherit';
   });
+
+
+/* lastfm js /*/
+
+var LFM_API = "https://ws.audioscrobbler.com/2.0/";
+var LFM_KEY = "11d3d143fcf0866bce70d5c1f495bc64"; // Get one at https://secure.last.fm/login?next=/api/account/create
+var LFM_USER = "vxnuaj";
+
+function getNowPlaying() {
+  var recentTracksUrl =
+    LFM_API+"?method=user.getrecenttracks&user="+LFM_USER+"&api_key="+LFM_KEY+"+&format=json&limit=1";
+
+  if (window.XMLHttpRequest) {
+      httpRequest = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+      httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        // All set
+        var response = JSON.parse(httpRequest.responseText);
+        console.log(response);
+        var currentTrack = response.recenttracks.track[0];
+
+        // Check if it's the same, if not then rerender
+        if (!window.nowPlaying || window.nowPlaying.mbid != currentTrack.mbid) {
+          window.nowPlaying = currentTrack;
+          renderNowPlaying(currentTrack);
+        }
+        setTimeout(getNowPlaying, 60*1000);
+      } else {
+        console.log('There was a problem with the last.fm request.');
+      }
+    }
+  };
+  httpRequest.open('GET', recentTracksUrl, true);
+  httpRequest.send();
+}
+
+
+var nowPlayingNode = null;
+
+function renderNowPlaying(track) {
+  console.log(track);
+  if (nowPlayingNode) {
+    nowPlayingNode.remove();
+  }
+  nowPlayingNode = document.createElement("a");
+  nowPlayingNode.setAttribute("class", "now-playing");
+
+  var imageurl = track.image.slice(-1)[0]["#text"];
+  var nowPlayingImage = document.createElement("img");
+  nowPlayingImage.setAttribute("src", imageurl);
+  nowPlayingNode.appendChild(nowPlayingImage);
+
+  // Add more stuff to the display
+
+  var currently = track["@attr"] && track["@attr"].nowplaying == "true";
+
+  var metadata = document.createElement("div");
+  metadata.setAttribute("class", "np-metadata");
+  metadata.innerHTML =
+    "<span class=\"np-heading\">" + (currently ? "Now Playing" : "Last Played ") + "</span>" +
+    "<span class=\"np-title\"><strong>"+track.name+"</strong></span>" + "<span> •</span>" +
+    "<span class=\"np-artist\"><i> "+track.artist["#text"]+"</i></span>"
+
+  nowPlayingNode.appendChild(metadata);
+
+  nowPlayingNode.setAttribute("href", track.url);
+
+  document.body.appendChild(nowPlayingNode);
+
+  setTimeout(function() {
+    nowPlayingNode.setAttribute("class", "now-playing loaded");
+  }, 100);
+}
+  
