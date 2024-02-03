a0d1e8d6e4589d59125ba8a3a9c6b6dfc7b289e0
vxnuaj <jv.100420@gmail.com>
2024-02-03 11:40:51 -0500

    Animation

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,32 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
-import utilStyles from '../styles/utils.module.css'
+import utilStyles from '../styles/utils.module.css';
+
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
         <title>vxnuaj.life</title>
         <link rel="icon" href="/me.ico" />
-        <link rel="stylsheet" href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@400;700&family=Source+Serif:wght@400;700&display=swap"></link>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@400;700&family=Source+Serif:wght@400;700&display=swap"></link>
       </Head>
 
       <main>
-        <h1 className='intro'>Hey, I'm Juan!</h1>
+        <h1 className='intro'>
+          Hey, I'm Juan!
+        </h1>
         <p className = 'des'>
         Currently, I’m building at the intersection of Brain Computer Interfaces & AI, with <br></br>
         a vision of creating the future of humanity at the bleeding edge of emerging technologies.
         </p>
-        <p className = 'des2'>
+        <p className = 'des1'>
           For the past year, I’ve been blessed to be part of <a href = "https://tks.world">The Knowledge Society</a>,<br></br>
           a human accelerator designed to maximize human potential.
         </p>
-        <p className = 'des3'>
+        <p className = 'des2'>
         I’m fascinated about philosophies of fulfillment, implications of Artificial
         <br></br>General Intelligence, theories of consciousness, and unlocking human potential.
         </p>
@@ -53,11 +57,56 @@ export default function Home() {
           left: 25px;
           width: fit-content;
           margin-bottom: 0px;
+          opacity: 0;
+          animation: fadeIn .8s ease-in-out;
+          animation-fill-mode: forwards;
         }
 
+        @keyframes fadeIn{
+          0% {opacity: 0;}
+          100% {opacity: 1;}
+        }
+
+
+        .des{ 
+          opacity: 0;
+          animation: fadeIn .8s ease-in-out;
+          animation-delay: 1.5s;
+          animation-fill-mode: forwards;
+        }
+
+        .des1{
+          opacity: 0;
+          animation: fadeIn .8s ease-in-out;
+          animation-delay: 4s;
+          animation-fill-mode: forwards;
+        }
+
+        .des2{
+          opacity: 0;
+          animation: fadeIn .8s ease-in-out;
+          animation-delay: 6.5s;
+          animation-fill-mode: forwards;
+        }
+
+        .updates{
+          opacity: 0;
+          animation: fadeIn .8s ease-in-out;
+          animation-delay: 9s;
+          animation-fill-mode: forwards;
+        }
+
+        .links {
+          opacity: 0;
+          animation: fadeIn .8s ease-in-out;
+          animation-delay: 11.5s;
+          animation-fill-mode: forwards;
+        }
+
+
         .des,
+        .des1,
         .des2,
-        .des3,
         .updates,
         .links {
           font-size: 16px;
